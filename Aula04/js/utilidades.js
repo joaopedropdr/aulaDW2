703bf437ade831bb0d53804a9f41cc0b3958f714
João Pedro Pascuci De Russi <joao.russi@fatec.sp.gov.br>
2025-09-08 12:42:55 -0300

    Aula 08-09

--- Aula04/js/utilidades.js
+++ Aula04/js/utilidades.js
@@ -5,14 +5,34 @@ function juntarNome() {
     let nome = document.getElementById('nome').value;
     let sobreNome = document.getElementById('sobrenome').value;
     // Concatenação
-    let nomeCompleto = nome + "" + sobreNome;
-    //Interpolação
-    let nomeCompleto1 = `<i>${nome} <br>${sobreNome}</i>`;
-    
-    // Injetando o novo texto no paragráfo
-    document.getElementById('nomecompleto').innerText = nomeCompleto;
-
-    document.getElementById('nomecompleto1').innerHTML = nomeCompleto1;
+    //let nomeCompleto = nome + "" + sobreNome;
+    if(!nome && !sobreNome) {
+        document.writeln("<p><strong>Nome e/ou sobrenome não foram informados!!!</strong></p>");
+    } else {
+        //Interpolação
+        let nomeCompleto = `${nome} ${sobreNome}`;
+        document.getElementById('nomecompleto').innerText = nomeCompleto;
+        
+        let nomeCompleto1 = `<i>${nome} <br>${sobreNome}</i>`;
+        document.getElementById('nomecompleto1').innerHTML = nomeCompleto1;
+    }
     
 }
+
+function janelaNome() {
+    let nome = prompt("Entre com seu nome: ");
+    let sobreNome = prompt("Entre com seu sobrenome: ");
+    
+    if(!nome && !sobreNome) {
+        document.writeln("<p><strong>Nome e/ou sobrenome não foram informados!!!</strong></p>");
+    } else {
+        let nomeCompleto = `${nome} ${sobreNome}`;
+        document.getElementById('nomecompleto').innerText = nomeCompleto;
+        
+        let nomeCompleto1 = `<i>${nome} <br>${sobreNome}</i>`;
+        document.getElementById('nomecompleto1').innerHTML = nomeCompleto1;
+    }
+
+}
+
 
