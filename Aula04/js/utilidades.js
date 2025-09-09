82c71c976dfa51ade8148baaba441f84d835b4a8
João Pedro Pascuci De Russi <joao.russi@fatec.sp.gov.br>
2025-09-09 09:14:08 -0300

    09-09

--- Aula04/js/utilidades.js
+++ Aula04/js/utilidades.js
@@ -1,6 +1,4 @@
-
 // Função para juntar nome e sobrenome
-
 function juntarNome() {
     let nome = document.getElementById('nome').value;
     let sobreNome = document.getElementById('sobrenome').value;
@@ -15,15 +13,15 @@ function juntarNome() {
         
         let nomeCompleto1 = `<i>${nome} <br>${sobreNome}</i>`;
         document.getElementById('nomecompleto1').innerHTML = nomeCompleto1;
-    }
-    
+    }  
 }
 
+// Função para pegar o nome e sobrenome no prompt
 function janelaNome() {
     let nome = prompt("Entre com seu nome: ");
     let sobreNome = prompt("Entre com seu sobrenome: ");
     
-    if(!nome && !sobreNome) {
+    if(!nome || !sobreNome) {
         document.writeln("<p><strong>Nome e/ou sobrenome não foram informados!!!</strong></p>");
     } else {
         let nomeCompleto = `${nome} ${sobreNome}`;
@@ -32,7 +30,19 @@ function janelaNome() {
         let nomeCompleto1 = `<i>${nome} <br>${sobreNome}</i>`;
         document.getElementById('nomecompleto1').innerHTML = nomeCompleto1;
     }
-
 }
 
+function ajustarFoco() {
+    document.getElementById("nome").focus();
+}
 
+function limparDados() {
+    // Excluido os arquivos sem usar o reset
+    // let nome = document.getElementById('nome').value = "";
+    // let sobreNome = document.getElementById('sobrenome').value = "";
+
+    document.getElementById("nomes").reset();
+
+    document.getElementById('nomecompleto').innerText = "";
+    document.getElementById('nomecompleto1').innerHTML = "";
+}
